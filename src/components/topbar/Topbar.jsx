6d2cce3cc1d1255a16ed41c2e13ba6fe4e279f43
KelvinIO <kelviinvn@protonmail.com>
2021-07-11 22:17:54 +1000

    Added icons to topbar, styled topbar text, added basic menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,30 @@
 import "./topbar.scss"
+import {Phone, Email} from "@material-ui/icons"
 
 export default function topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">Kelvin.IO</a>
+                    <div className="itemContainer">
+                        <Phone className="icon"/>
+                        <span>0492 905 795</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Email className="icon"/>
+                        <span>kelviinvn@protonmail.com</span>
+                    </div>
+                </div>
+
+                <div className="right">
+                    <div className="menu">
+                        <span className="line1"></span>
+                        <span className="line2"></span>
+                        <span className="line3"></span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
