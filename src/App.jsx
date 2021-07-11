9c73196eafaaa3dabd0c3d8867890d3b70471e63
KelvinIO <kelviinvn@protonmail.com>
2021-07-11 18:37:53 +1000

    Added sections to app.jsx, added base styling to topbar section

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+      <Intro/>
+      <Portfolio/>
+      <Projects/>
+      <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
