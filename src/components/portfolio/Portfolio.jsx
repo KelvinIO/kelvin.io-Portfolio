acb96881022d824c29d4aaa37d316024f459a85c
KelvinIO <kelviinvn@protonmail.com>
2021-07-13 17:39:52 +1000

    Added portfolio section with basic placeholders - modify data.js to fill placeholders

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,71 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList"
 import "./portfolio.scss"
+import { 
+    featuredPortfolio,
+    webPortfolio,
+    desktopPortfolio,
+    ph1Portfolio,
+    ph2Portfolio,
+ } from "../../data"
 
-export default function portfolio() {
+export default function Portfolio() {
+
+    const [ selected, setSelected ] = useState("featured");
+    const [ data, setData ] = useState([]);
+
+    const menuList = [
+        { id: "featured", title: "Featured" },
+        { id: "web", title: "Web Apps" },
+        { id: "desktop", title: "Desktop Apps" },
+        { id: "ph1", title: "Placeholder #1" },
+        { id: "ph2", title: "Placeholder #2" },
+    ];
+
+    useEffect(() => {
+        switch(selected){
+            case "featured":
+                setData(featuredPortfolio);
+                break;
+            case "web":
+                setData(webPortfolio);
+                break;
+            case "desktop":
+                setData(desktopPortfolio);
+                break;
+            case "ph1":
+                setData(ph1Portfolio);
+                break;
+            case "ph2":
+                setData(ph2Portfolio);
+                break;
+            default:
+                setData(featuredPortfolio)
+        }
+    }, [selected])
+
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                {menuList.map(item=>(
+                <PortfolioList 
+                title={item.title} 
+                active={selected === item.id} 
+                setSelected={setSelected}
+                id={item.id}/>
+                ))}
+            </ul>
+            <div className="container">
+                {data.map(d=>(
+                    <div className="item">
+                        <img src={d.img} alt=""/>
+                        <h3>{d.title}</h3>
+                    </div>
+                ))}
+
+
+            </div>
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const featuredPortfolio = [
+    { id: "1", title: "Pathfinding ALgorithm Visualizer", img: "https://www.bigw.com.au/medias/sys_master/images/images/h7b/he8/11881557458974.jpg" },
+    { id: "2", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+    { id: "3", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+    { id: "4", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+    { id: "5", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+    { id: "6", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+]
+
+export const webPortfolio = [
+    { id: "1", title: "WebApp1", img: "../public/assets/sec2project1.png" },
+    { id: "2", title: "WebApp1", img: "../public/assets/sec2project1.png" },
+    { id: "3", title: "WebApp1", img: "../public/assets/sec2project1.png" },
+    { id: "4", title: "WebApp1", img: "../public/assets/sec2project1.png" },
+    { id: "5", title: "WebApp1", img: "../public/assets/sec2project1.png" },
+    { id: "6", title: "WebApp1", img: "../public/assets/sec2project1.png" },
+]
+
+export const desktopPortfolio = [
+    { id: "1", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+    { id: "2", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+    { id: "3", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+    { id: "4", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+    { id: "5", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+    { id: "6", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec1project1.png" },
+]
+
+export const ph1Portfolio = [
+    { id: "1", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+    { id: "2", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+    { id: "3", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+    { id: "4", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+    { id: "5", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+    { id: "6", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+]
+
+export const ph2Portfolio = [
+    { id: "1", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+    { id: "2", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+    { id: "3", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+    { id: "4", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+    { id: "5", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+    { id: "6", title: "Pathfinding ALgorithm Visualizer", img: "../public/assets/sec2project1.png" },
+]
