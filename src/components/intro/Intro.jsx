2c805bce78000c785dcf6acb9c0da72483deb16b
KelvinIO <kelviinvn@protonmail.com>
2021-07-13 14:18:17 +1000

    Added itypedjs library for dynamic text in intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,20 @@
-import "./intro.scss"
+import "./intro.scss";
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
-export default function intro() {
+export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            strings: ["Programmer", "Developer", "Engineer"],
+            backSpeed: 60,
+            backDelay: 1500,
+        });
+    }, []);
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -12,7 +26,7 @@ export default function intro() {
                 <div className="wrapper">
                     <h2>Hey there! I'm</h2>
                     <h1>Kelvin</h1>
-                    <h3>Software<span></span></h3>
+                    <h3>Software <span ref={textRef}></span></h3>
                 </div>
                 <a href="#portfolio">
                     <img src="assets/darrow.png" alt="" />
