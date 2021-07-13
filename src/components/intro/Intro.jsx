04482999cc69df166a0b1aa59a009b2d4e0bcfb5
KelvinIO <kelviinvn@protonmail.com>
2021-07-13 13:51:51 +1000

    Added basic intro page with placeholder imgs

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,21 @@ import "./intro.scss"
 export default function intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/self.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hey there! I'm</h2>
+                    <h1>Kelvin</h1>
+                    <h3>Software<span></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/darrow.png" alt="" />
+                </a>
+            </div>
         </div>
     )
 }
